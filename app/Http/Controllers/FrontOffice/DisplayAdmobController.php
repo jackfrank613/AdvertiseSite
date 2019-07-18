3da4aafd5da7778faac4136d02de5491e12ff5ba
jack <jackfrank613@gamil.com>
2019-07-19 04:55:38 +0800

    fetch data from database

--- app/Http/Controllers/FrontOffice/DisplayAdmobController.php
+++ app/Http/Controllers/FrontOffice/DisplayAdmobController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\FrontOffice;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\PostedAdmob;
 class DisplayAdmobController extends Controller
 {
     //
 
     public function getDisplaypage(){
-        return view('frontoffice.home.displayadmob');
+
+        $result=PostedAdmob::orderBy('jamii_postedadmob.id','asc')->limit(10)->leftJoin('jamii_subcategory','jamii_postedadmob.sub_id','=','jamii_subcategory.id')->get()->toarray();
+        $count=count($result);
+        //  print_r($result);exit;
+        return view('frontoffice.home.displayadmob')->with(compact('result', 'count'));
     }
 }
